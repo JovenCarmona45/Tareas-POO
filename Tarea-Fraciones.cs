38fb19cac0f2fbab44d86ef0e074831484606c1e
JovenCarmona45 <47642007+JovenCarmona45@users.noreply.github.com>
2019-02-25 23:30:36 -0800

    Add files via upload

---
+++ Tarea-Fraciones.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Consola_12
+{
+    class Frac
+    {
+        private int Num;
+        private int Dem;
+
+        public Frac (int Num, int Dem)
+        {
+            this.Num = Num; this.Dem = Dem;
+        }
+        public Frac()
+        {
+            Num = 1;
+            Dem = 1;
+        }
+
+        public static Frac operator + (Frac X, Frac Y)
+        {
+            return new Frac(X.Num * Y.Dem + X.Dem * Y.Num, X.Dem * Y.Dem);
+        }
+        public static Frac operator - (Frac X, Frac Y)
+        {
+            return new Frac(X.Num * Y.Dem - X.Dem * Y.Num, X.Dem * Y.Dem);
+        }
+        public static Frac operator * (Frac X, Frac Y)
+        {
+            return new Frac(X.Num * Y.Num, X.Dem * Y.Dem);
+        }
+        public static Frac operator /(Frac X, Frac Y)
+        {
+            return new Frac(X.Num * Y.Dem, X.Dem * Y.Num);
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine("------------\n" + "{0}/{1}\n" + "------------\n", Num, Dem);
+        }
+
+        public decimal A_decimal()
+        {
+            return Num / Dem;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Frac a = new Frac(1, 2);
+            Frac b = new Frac(1, 3);
+            Frac c = a + b;
+            Frac d = a - b;
+            Frac e = a * b;
+            Frac f = a / b;
+            a.Imprime();
+            b.Imprime();
+            Console.WriteLine("Suma");
+            c.Imprime();
+            Console.WriteLine("Resta");
+            d.Imprime();
+            Console.WriteLine("Multiplicación");
+            e.Imprime();
+            Console.WriteLine("Division");
+            f.Imprime();
+            Console.ReadKey();
+        }
+    }
+}
